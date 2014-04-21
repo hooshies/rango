f96639629a2ebcc18ce97320e43dd82ad499b63c
hooshies <hooshies@gmail.com>
2014-04-21 11:48:06 -0400

    ch 4 with exercises

--- rango/views.py
+++ rango/views.py
@@ -10,4 +10,6 @@ def index(request):
     return render_to_response('rango/index.html',context_dict,context)
 
 def about(request):
-    return HttpResponse("Rango says: Here is the about page. <a href='/rango/'>Index</a>")
+    context = RequestContext(request)
+    context_dict = {'aboutmessage': "This is the about message"}
+    return render_to_response('rango/about.html',context_dict,context)
