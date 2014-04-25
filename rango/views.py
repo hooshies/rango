ac0e98acdf27c9a5ff632d06598eb0ef7afc1d0d
hooshies <hooshies@gmail.com>
2014-04-25 16:40:54 -0400

    ch 6

--- rango/views.py
+++ rango/views.py
@@ -2,14 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'pages': page_list}
+    for category in category_list:
+        category.url = category.name.replace(' ','_')
     return render_to_response('rango/index.html',context_dict,context)
 
 def about(request):
     context = RequestContext(request)
     context_dict = {'aboutmessage': "This is the about message"}
     return render_to_response('rango/about.html',context_dict,context)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = category_name_url.replace('_',' ')
+
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -10,6 +10,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'tango_with_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    #delete first entry later
+    url(r'^$', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 )
